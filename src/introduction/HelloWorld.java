ffea8cc5da15686820330d4569fddbd2f319f9fe
Veer.Patil <veer.1109@gmail.com>
2018-12-03 18:48:12 +0530

    Initial Files for Java

---
+++ src/introduction/HelloWorld.java
@@ -0,0 +1,12 @@
+package introduction;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//This is my first java program
+		System.out.println("Hello World");
+
+	}
+
+}
