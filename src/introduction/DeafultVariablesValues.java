8c6512e192be206ed3285f6e556cc85c0b8a8445
Veer.Patil <veer@ash-software.com>
2018-12-03 19:07:43 +0530

    Default values of Variables

---
+++ src/introduction/DeafultVariablesValues.java
@@ -0,0 +1,42 @@
+package introduction;
+
+public class DeafultVariablesValues {
+
+	static byte byteVariable; 
+	static short shortVariable;
+	static int integerVariable;
+	static long longVariable;
+	static float floatVariable;
+	static double doubleVariable;
+	static boolean booleanVariable; 
+	static char charVariable;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	
+		System.out.println("Byte Value "+ byteVariable);
+		
+
+		System.out.println("Short Value"+ shortVariable);
+		
+		
+		System.out.println("Int Value"+ integerVariable);
+		
+		
+		System.out.println("Long Value"+ longVariable);
+		
+		
+		System.out.println("Float Variable"+ floatVariable);
+
+		
+		
+		System.out.println("Double Variable:"+ doubleVariable);
+		
+	
+		System.out.println("Boolean Variable:" + booleanVariable);
+		
+		
+		System.out.println("Character Value:"+ charVariable);
+	}
+
+}
