11cd3c0d2ff3ea6101893010a758df22214a80bd
Veer.Patil <veer@ash-software.com>
2018-12-03 19:02:21 +0530

    Added Data Types demo file

---
+++ src/introduction/Variables.java
@@ -0,0 +1,39 @@
+package introduction;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/* Byte
+		 * Min:-128
+		 * MaX: 127
+		 * Dafault:0
+		 */
+		byte byteVariable=100; 
+		System.out.println("Byte Value "+ byteVariable);
+		
+		short shortVariable=100;
+		System.out.println("Short Value"+ shortVariable);
+		
+		int integerVariable=100000;
+		System.out.println("Int Value"+ integerVariable);
+		
+		long longVariable=100000000000L;
+		System.out.println("Long Value"+ longVariable);
+		
+		float floatVariable= 3.14F;
+		System.out.println("Float Variable"+ floatVariable);
+
+		double doubleVariable=63.33;
+		
+		System.out.println("Double Variable:"+ doubleVariable);
+		
+		boolean booleanVariable= false; 
+		System.out.println("Boolean Variable:" + booleanVariable);
+		
+		char charVariable='C';
+		System.out.println("Character Value:"+ charVariable);
+	}
+
+}
